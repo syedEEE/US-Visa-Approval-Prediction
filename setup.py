06c779580fb8c9136e1c807782b50b33156cf0c6
Syed Mohamed Harris P S <syedmdharris@gmail.com>
2025-04-16 19:23:58 +0530

    MongoDB_Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Syed",
+    author_email="syedmdharris@gmail.com",
+    packages=find_packages()
+)
